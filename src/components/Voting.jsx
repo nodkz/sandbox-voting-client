bd7eccbec4d44d41864a6c524f3aa8f96e785ac1
nodkz <nodkz@mail.ru>
2015-10-05 11:52:41 +0600

    Wired up redux with dumb components via connect. Write initial reducer.

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,8 +1,9 @@
 import React from 'react/addons';
+import {connect} from 'react-redux';
 import Vote from './Vote.jsx';
 import Winner from './Winner.jsx';
 
-export default React.createClass({
+export const Voting = React.createClass({
   mixins: [React.addons.PureRenderMixin],
   render: function() {
     return <div className="voting">
@@ -13,3 +14,12 @@ export default React.createClass({
     </div>;
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    winner: state.get('winner')
+  };
+}
+
+export const VotingContainer = connect(mapStateToProps)(Voting);
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import Router, {Route, DefaultRoute} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer'
 import App from './components/App';
-import Voting from './components/Voting';
-import Results from './components/Results';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
+
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+});
 
 
 const routes = <Route handler={App}>
-  <Route path="/results" handler={Results} />
-  <DefaultRoute handler={Voting} />
+  <Route path="/results" handler={ResultsContainer} />
+  <DefaultRoute handler={VotingContainer} />
 </Route>;
 
 Router.run(routes, (Root) => {
   React.render(
-    <Root />,
+    <Provider store={store}>
+      {() => <Root />}
+    </Provider>,
     document.getElementById('app')
   )
 });
